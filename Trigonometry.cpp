a7836b0a4aa385b305a24d68cbb4c1e8c799f533
mochamethod <mochamethod@gmail.com>
2019-11-03 12:20:35 -0700

    Added example triangle in comments, and created basic trig problems to solve. Also, updated ints to floats for decimal precision.

--- Trigonometry.cpp
+++ Trigonometry.cpp
@@ -1,25 +1,66 @@
 #include <iostream>
+#include <math.h>
 
-int sin(int opp, int hyp)
+float fsin(float opp, float hyp)
 {
 	return opp / hyp;
 }
 
-int cos(int adj, int hyp)
+float fcos(float adj, float hyp)
 {
 	return adj / hyp;
 }
 
-int tan(int opp, int adj)
+float ftan(float opp, float adj)
 {
 	return opp / adj;
 }
 
+float pythag(float a, float b) 
+{
+	float c = (a * a) + (b * b);
+	return (float)sqrt(c);
+}
+
 int main()
 {
-	sin(4, 5);
-	cos(3, 5);
-	tan(4, 3);
+	/*
+	Right triangle whose sides are named a, b and c and angles x (the right angle), y, and z.
+
+		|\
+		|y\
+		|  \	
+	a=7	|	\ c
+		|	 \
+		|_	  \
+		|x|___z\
+
+		  b=4
+	*/
+
+	float a = 7.0f;
+	float b = 4.0f;
+
+	// Problem: right triangle where a = 7 and b = 4; what is c?
+	// Solution: the sqrt(65)
+	float c = pythag(a, b);
+
+	// Problem: what is the cos of angle z?
+	// Solution: 4/sqrt(65) or (4*sqrt(65))/65
+	float cos_z = fcos(b, c);
+
+	// Problem: what is the sin of angle z?
+	// Solution: 7/sqrt(65) or (7*sqrt(65))/65
+	float sin_z = fsin(a, c);
+
+	// Problem: what is the tan of angle z?
+	// Solution: 7/4
+	float tan_z = ftan(a, b);
+
+	char buff[100];
+	snprintf(buff, sizeof(buff), "A: %f,\nB: %f,\nC: %f,\ncos angle Z: %f,\nsin angle Z: %f,\ntan angle Z: %f\n", a, b, c, cos_z, sin_z, tan_z);
+	std::string buffAsStdStr = buff;
+	std::cout << buffAsStdStr << "\n";
 
 	return 0;
 }
