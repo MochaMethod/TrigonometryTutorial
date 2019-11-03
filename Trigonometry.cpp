8bff275803f1f109216458eb20e5984649a8f394
mochamethod <mochamethod@gmail.com>
2019-11-03 11:19:37 -0700

    Implemented sin, cos, and tan functions.

--- Trigonometry.cpp
+++ Trigonometry.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
+int sin(int opp, int hyp)
+{
+	return opp / hyp;
+}
+
+int cos(int adj, int hyp)
+{
+	return adj / hyp;
+}
+
+int tan(int opp, int adj)
+{
+	return opp / adj;
+}
+
 int main()
 {
+	sin(4, 5);
+	cos(3, 5);
+	tan(4, 3);
+
 	return 0;
 }
